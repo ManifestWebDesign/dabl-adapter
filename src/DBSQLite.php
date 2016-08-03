dc5abf22a09675862e582873f0a470ca3818865e
Cole Hafner <cole@manifestwebdesign.com>
2016-08-02 21:46:12 -0700

    whitespace formatting

--- src/DBSQLite.php
+++ src/DBSQLite.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace Dabl\Adapter;
+
 use Dabl\Adapter\Propel\Model\Database;
 use Dabl\Adapter\Propel\Platform\SqlitePlatform;
 use Dabl\Adapter\Propel\Reverse\SqliteSchemaParser;
@@ -39,9 +40,9 @@ class DBSQLite extends DABLPDO {
 	 */
 	function weekStart($date) {
 		return "CASE DATE($date, 'weekday 0') "
-				. "WHEN DATE($date) THEN DATE($date) "
-				. "ELSE DATE($date, 'weekday 0', '-7 days') "
-				. "END";
+			. "WHEN DATE($date) THEN DATE($date) "
+			. "ELSE DATE($date, 'weekday 0', '-7 days') "
+			. "END";
 	}
 
 	/**
@@ -77,8 +78,8 @@ class DBSQLite extends DABLPDO {
 	 * For SQLite this method has no effect, since SQLite doesn't support specifying a character
 	 * set (or, another way to look at it, it doesn't require a single character set per DB).
 	 *
-	 * @param	  string The charset encoding.
-	 * @throws	 Exception If the specified charset doesn't match sqlite_libencoding()
+	 * @param      string The charset encoding.
+	 * @throws     Exception If the specified charset doesn't match sqlite_libencoding()
 	 */
 	function setCharset($charset) {
 	}
@@ -86,8 +87,8 @@ class DBSQLite extends DABLPDO {
 	/**
 	 * This method is used to ignore case.
 	 *
-	 * @param	  in The string to transform to upper case.
-	 * @return	 The upper case string.
+	 * @param      in The string to transform to upper case.
+	 * @return     The upper case string.
 	 */
 	function toUpperCase($in) {
 		return 'UPPER(' . $in . ')';
@@ -96,8 +97,8 @@ class DBSQLite extends DABLPDO {
 	/**
 	 * This method is used to ignore case.
 	 *
-	 * @param	  in The string whose case to ignore.
-	 * @return	 The string in a case that can be ignored.
+	 * @param      in The string whose case to ignore.
+	 * @return     The string in a case that can be ignored.
 	 */
 	function ignoreCase($in) {
 		return 'UPPER(' . $in . ')';
@@ -106,9 +107,9 @@ class DBSQLite extends DABLPDO {
 	/**
 	 * Returns SQL which concatenates the second string to the first.
 	 *
-	 * @param	  string String to concatenate.
-	 * @param	  string String to append.
-	 * @return	 string
+	 * @param      string String to concatenate.
+	 * @param      string String to append.
+	 * @return     string
 	 */
 	function concatString($s1, $s2) {
 		return "($s1 || $s2)";
@@ -117,10 +118,10 @@ class DBSQLite extends DABLPDO {
 	/**
 	 * Returns SQL which extracts a substring.
 	 *
-	 * @param	  string String to extract from.
-	 * @param	  int Offset to start from.
-	 * @param	  int Number of characters to extract.
-	 * @return	 string
+	 * @param      string String to extract from.
+	 * @param      int Offset to start from.
+	 * @param      int Number of characters to extract.
+	 * @return     string
 	 */
 	function subString($s, $pos, $len) {
 		return "substr($s, $pos, $len)";
@@ -129,25 +130,25 @@ class DBSQLite extends DABLPDO {
 	/**
 	 * Returns SQL which calculates the length (in chars) of a string.
 	 *
-	 * @param	  string String to calculate length of.
-	 * @return	 string
+	 * @param      string String to calculate length of.
+	 * @return     string
 	 */
 	function strLength($s) {
 		return "length($s)";
 	}
 
 	/**
-	 * @see		DABLPDO::applyLimit()
+	 * @see        DABLPDO::applyLimit()
 	 */
 	function applyLimit(&$sql, $offset, $limit) {
-		if ( $limit > 0 ) {
+		if ($limit > 0) {
 			$sql .= " LIMIT " . $limit . ($offset > 0 ? " OFFSET " . $offset : "");
-		} elseif ( $offset > 0 ) {
+		} elseif ($offset > 0) {
 			$sql .= " LIMIT -1 OFFSET " . $offset;
 		}
 	}
 
-	function random($seed=NULL) {
+	function random($seed = NULL) {
 		return 'random()';
 	}
 
